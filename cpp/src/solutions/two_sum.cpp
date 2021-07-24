4ff9c829b20199e24a6caf7b64fd7f1d172863a6
LazyWolfLin <LazyWolfLin@gmail.com>
2021-07-24 16:07:55 +0800

    Add cpp example.

---
+++ cpp/src/solutions/two_sum.cpp
@@ -0,0 +1,49 @@
+// LeetCode 1. Two Sum in C++
+
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> indices;
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            auto iter = indices.find(target - nums[i]);
+            if (iter != indices.end())
+                return {iter->second, static_cast<int>(i)};
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+TEST(twoSum, example1)
+{
+    Solution s;
+    vector<int> nums{2, 7, 11, 15}, ans{0, 1};
+    int target = 9;
+    EXPECT_EQ(s.twoSum(nums, target), ans);
+}
+
+TEST(twoSum, example2)
+{
+    Solution s;
+    vector<int> nums{3, 2, 4}, ans{1, 2};
+    int target = 6;
+    EXPECT_EQ(s.twoSum(nums, target), ans);
+}
+
+TEST(twoSum, example3)
+{
+    Solution s;
+    vector<int> nums{3, 3}, ans{0, 1};
+    int target = 6;
+    EXPECT_EQ(s.twoSum(nums, target), ans);
+}

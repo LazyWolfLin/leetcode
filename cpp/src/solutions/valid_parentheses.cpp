731516713755c839b0507f00198862375754a8c8
LazyWolfLin <LazyWolfLin@gmail.com>
2021-12-23 23:09:08 +0800

    Solve "Valid Parentheses".

--- cpp/src/solutions/valid_parentheses.cpp
+++ cpp/src/solutions/valid_parentheses.cpp
@@ -6,17 +6,15 @@ class Solution {
  public:
   bool isValid(string s) {
     stack<char> stack;
-    for (auto i = 0; i < s.size(); i++) {
-      if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-        stack.push(s[i]);
+    for (auto ch : s) {
+      if (ch == '(' || ch == '{' || ch == '[') {
+        stack.push(ch);
+      } else if (!stack.empty() && ((ch == ')' && stack.top() == '(') ||
+                                    (ch == '}' && stack.top() == '{') ||
+                                    (ch == ']' && stack.top() == '['))) {
+        stack.pop();
       } else {
-        if (stack.empty()) return false;
-        if ((s[i] == ')' && stack.top() == '(') ||
-            (s[i] == '}' && stack.top() == '{') ||
-            (s[i] == ']' && stack.top() == '['))
-          stack.pop();
-        else
-          return false;
+        return false;
       }
     }
     return stack.empty();
---
+++ rust/src/solutions/valid_parentheses.rs
@@ -0,0 +1,67 @@
+// LeetCode 20. Valid Parentheses in Rust
+
+use super::Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for ch in s.chars() {
+            match ch {
+                '(' | '{' | '[' => {
+                    stack.push(ch);
+                }
+                ')' if stack.last() == Some(&'(') => {
+                    stack.pop();
+                }
+                '}' if stack.last() == Some(&'{') => {
+                    stack.pop();
+                }
+                ']' if stack.last() == Some(&'[') => {
+                    stack.pop();
+                }
+                _ => return false,
+            }
+        }
+        stack.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn example1() {
+        let s = String::from("()");
+        let ans = true;
+        assert_eq!(Solution::is_valid(s), ans);
+    }
+
+    #[test]
+    fn example2() {
+        let s = String::from("()[]{}");
+        let ans = true;
+        assert_eq!(Solution::is_valid(s), ans);
+    }
+
+    #[test]
+    fn example3() {
+        let s = String::from("(]");
+        let ans = false;
+        assert_eq!(Solution::is_valid(s), ans);
+    }
+
+    #[test]
+    fn example4() {
+        let s = String::from("([)]");
+        let ans = false;
+        assert_eq!(Solution::is_valid(s), ans);
+    }
+
+    #[test]
+    fn example5() {
+        let s = String::from("{[]}");
+        let ans = true;
+        assert_eq!(Solution::is_valid(s), ans);
+    }
+}
